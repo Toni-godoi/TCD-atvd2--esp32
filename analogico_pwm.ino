aa888dd0cfa279393c909d974bda58167b3f1df1
ToniGodoi <106675661+Toni-godoi@users.noreply.github.com>
2026-04-23 01:22:51 -0400

    Add PWM control based on analog input

---
+++ analogico_pwm.ino
@@ -0,0 +1,21 @@
+// analogico_pwm.ino
+#define PINO_POT 34
+#define PINO_LED 25
+#define CANAL_PWM 0
+#define FREQ_PWM 1000 // Hz
+#define RESOLUCAO 8 // bits → 0 a 255
+
+void setup() {
+ Serial.begin(115200);
+ ledcAttachChannel(PINO_LED, FREQ_PWM, RESOLUCAO, CANAL_PWM);
+}
+void loop() {
+ int leitura = analogRead(PINO_POT); // 0 a 4095 (12 bits)
+ int pwm = map(leitura, 0, 4095, 0, 255); // remapeia para 8 bits
+ ledcWrite(PINO_LED, pwm); // ← correção aqui
+ float tensao = leitura * (3.3 / 4095.0);
+ Serial.print("ADC: "); Serial.print(leitura);
+ Serial.print(" | V: "); Serial.print(tensao, 2);
+ Serial.print(" | PWM: "); Serial.println(pwm);
+ delay(200);
+}
